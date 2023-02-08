f17a6be34731347add845a4b942477a1a4910f78
Dinesh Kumar <dinesh08143@gmail.com>
2023-02-08 12:51:40 +0530

    2nd file in shell script

---
+++ second.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# This is the 2nd file in shellscripting -Varibles
+
+echo " Enter name :"
+read Enter
+name=$Enter
+
+
+echo " He is a software engineer trainee he is $name 
+       $name and 23 others are training for DevOps
+       $name is staying in hyderabad" 
+
